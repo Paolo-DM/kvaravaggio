4535d3404bd8d70582f0fc079bc3166f4e063c66
Paolo Di Martino <paolo.dimartino@outlook.it>
2023-04-28 14:44:40 +0200

    exported images for tutorial screen

---
+++ utils/tutorialUtils.tsx
@@ -0,0 +1,9 @@
+// Images
+const screen1 = require("../assets/images/tutorial/screen1.png");
+const screen2 = require("../assets/images/tutorial/screen2.png");
+const screen3 = require("../assets/images/tutorial/screen3.png");
+const screen4 = require("../assets/images/tutorial/screen4.png");
+const skipIcon = require("../assets/images/tutorial/skip.png");
+
+export const tutorialImages = [screen1, screen2, screen3, screen4];
+export const skipTutorialIcon = skipIcon;
