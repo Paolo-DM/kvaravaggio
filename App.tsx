b6a2468d79c7fb558c1517376406edf3a380779e
Paolo Di Martino <paolo.dimartino@outlook.it>
2023-05-01 15:53:09 +0200

    removed loading spinner, added splash screen logic with expo-splash-screen, rendering JSX only if appIsReady is true

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 // React
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 // Expo
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
+import * as SplashScreen from "expo-splash-screen";
 
 // React Native
-import { StyleSheet, View, ActivityIndicator } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 // Navigation
 import { NavigationContainer } from "@react-navigation/native";
@@ -25,65 +26,91 @@ const Tab = createBottomTabNavigator();
 
 interface State {
   tutorialCompleted: boolean | null;
+  appIsReady: boolean;
 }
 
 const initialState: State = {
   tutorialCompleted: null,
+  appIsReady: false,
 };
+
+SplashScreen.preventAutoHideAsync();
 
 const App = () => {
   const [state, setState] = useState<State>(initialState);
 
+  const checkTutorialStatus = async (): Promise<boolean> => {
+    try {
+      const tutorialCompleted = await getStorage("tutorialCompleted");
+      console.log("tutorialCompleted", tutorialCompleted);
+      return tutorialCompleted;
+    } catch (error) {
+      console.log("error", error);
+      return false;
+    }
+  };
+
   useEffect(() => {
-    checkTutorialStatus();
+    async function prepare() {
+      let tutorialCompleted = false;
+
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        tutorialCompleted = await checkTutorialStatus();
+
+        // Artificially delay for two seconds to simulate a slow loading
+        // await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.log("App loading error:");
+      } finally {
+        // Tell the application to render
+        setState({ ...state, appIsReady: true, tutorialCompleted });
+      }
+    }
+
+    prepare();
   }, []);
 
-  const checkTutorialStatus = async (): Promise<void> => {
-    const tutorialStatus: any = await getStorage("tutorialCompleted");
-    console.log("tutorialStatus", tutorialStatus);
-    const tutorialCompleted = tutorialStatus === true ? true : false;
-    console.log("tutorialCompleted", tutorialCompleted);
-    setState({ ...state, tutorialCompleted });
-  };
+  const onLayoutRootView = useCallback(async () => {
+    if (state.appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [state.appIsReady]);
+
+  if (!state.appIsReady) {
+    return null;
+  }
 
   return (
     <>
-      {(state.tutorialCompleted === null ||
-        state.tutorialCompleted === undefined) && (
-        <View
-          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        >
-          <ActivityIndicator size="large" />
-        </View>
-      )}
-      {(state.tutorialCompleted === true ||
-        state.tutorialCompleted === false) && (
-        <SafeAreaView style={styles.container}>
-          <View style={styles.container}>
-            <StatusBar style={"light"} backgroundColor="#272c34" />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.container} onLayout={onLayoutRootView}>
+          <StatusBar style={"light"} backgroundColor="#272c34" />
 
-            <NavigationContainer>
-              <Tab.Navigator
-                screenOptions={screenOptions}
-                initialRouteName={
-                  state.tutorialCompleted === true
-                    ? "Drawing Board"
-                    : "Tutorial"
-                }
-              >
-                <Tab.Group screenOptions={{ headerShown: false }}>
-                  <Tab.Screen
-                    name="Drawing Board"
-                    component={DrawingBoardScreen}
-                  />
-                  <Tab.Screen name="Gallery" component={GalleryScreen} />
-                  <Tab.Screen name="Tutorial" component={Tutorial} />
-                </Tab.Group>
-              </Tab.Navigator>
-            </NavigationContainer>
-          </View>
-        </SafeAreaView>
-      )}
+          <NavigationContainer>
+            <Tab.Navigator
+              screenOptions={screenOptions}
+              initialRouteName={
+                state.tutorialCompleted === true ? "Drawing Board" : "Tutorial"
+              }
+            >
+              <Tab.Group screenOptions={{ headerShown: false }}>
+                <Tab.Screen
+                  name="Drawing Board"
+                  component={DrawingBoardScreen}
+                />
+                <Tab.Screen name="Gallery" component={GalleryScreen} />
+                <Tab.Screen name="Tutorial" component={Tutorial} />
+              </Tab.Group>
+            </Tab.Navigator>
+          </NavigationContainer>
+        </View>
+      </SafeAreaView>
     </>
   );
 };
